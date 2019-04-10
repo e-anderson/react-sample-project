ae8c81975bbf2577f22bbe97b77cb356a85acc5d
Erica Anderson <anderson.erica.lee@gmail.com>
2019-04-09 21:37:42 -0700

    can add new items custom

--- src/Components/ToDoList/AddNewItem/AddNewItemComponent.js
+++ src/Components/ToDoList/AddNewItem/AddNewItemComponent.js
@@ -5,18 +5,24 @@ import { ListItemData } from '../ListItemData';
 export class AddNewItemComponent extends React.Component {
   constructor(props) {
     super(props);
+    this.state = { inputvalue: ''};
     this.handleClick = this.handleClick.bind(this);
   }
 
   handleClick() {
-    this.props.addItemCallback(new ListItemData(1, "stuff", false));
+    this.props.addItemCallback(new ListItemData(1, this.state.inputvalue, false));
+    this.refs.inputField.value = "";
+  }
+
+  updateInputValue = (evt) => {
+      this.setState({inputvalue: evt.target.value});
   }
 
   render() {
     return (
       <div className="InputContainer">
         <span className="InputPrompt">Enter a new task:</span>
-        <input type="text" />
+        <input ref="inputField" type="text" value={this.state.inputValue} onChange={this.updateInputValue} />
         <button onClick={this.handleClick}>Add</button>
       </div>
     )
